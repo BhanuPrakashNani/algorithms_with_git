7849ee4853aac61a73f1c1a3599be4a2c9a91773
bhanuprakashnani <p.bhanuprakash12345@gmail.com>
2018-12-12 12:49:23 +0530

    added the implementation of cycle detection in java according to the oop concepts

---
+++ algorithms/Linked_List/detect_loop_in_linkedlist/Driver.java
@@ -0,0 +1,11 @@
+public class Driver {
+
+		public static void main(String[]args) {
+			LinkedList l = new LinkedList();	//create object of the class Linkedlist
+			
+		   //insert the elements
+		   
+		   l.isCycle();
+		    
+		}
+	}
